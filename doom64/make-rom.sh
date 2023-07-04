4d48b1679af6712838e22f6f63bcea5e83227254
Immorpher <58839754+Immorpher@users.noreply.github.com>
2023-07-04 13:01:32 -0900

    Compiler O3 Optimization Update
    Recent menu changes have allowed this to compile and run on hardware at O3 optimization level

--- doom64/make-rom.sh
+++ doom64/make-rom.sh
@@ -11,13 +11,13 @@ mips-n64-ld -r -b binary ./Data/DOOM64.WSD -o ./Data/DOOM64.WSD.o
 mips-n64-ld -r -b binary ./Data/DOOM64.WDD -o ./Data/DOOM64.WDD.o
 
 mkdir build
-mips-n64-gcc -c -G0 -O2 -g -mabi=32 -ffreestanding -mfix4300 -I/usr/include/n64 -I/usr/include/n64/PR -Iinclude -Ibuild -Ibuild/include -Isrc \
+mips-n64-gcc -c -G0 -O3 -g -mabi=32 -ffreestanding -mfix4300 -I/usr/include/n64 -I/usr/include/n64/PR -Iinclude -Ibuild -Ibuild/include -Isrc \
 -D_FINALROM=1 -DNDEBUG=1 -DF3DEX_GBI_2=1 -MMD -MF build/cfb.d  -o build/cfb.o cfb.c
 
-mips-n64-gcc -c -G0 -O2 -g -mabi=32 -ffreestanding -mfix4300 -I/usr/include/n64 -I/usr/include/n64/PR -Iinclude -Ibuild -Ibuild/include -Isrc \
+mips-n64-gcc -c -G0 -O3 -g -mabi=32 -ffreestanding -mfix4300 -I/usr/include/n64 -I/usr/include/n64/PR -Iinclude -Ibuild -Ibuild/include -Isrc \
 -D_FINALROM=1 -DNDEBUG=1 -DF3DEX_GBI_2=1 -MMD -MF build/audio_heap.d  -o build/audio_heap.o audio_heap.c
 
-mips-n64-gcc -c -G0 -O2 -g -mabi=32 -ffreestanding -mfix4300 -I/usr/include/n64 -I/usr/include/n64/PR -Iinclude -Ibuild -Ibuild/include -Isrc \
+mips-n64-gcc -c -G0 -O3 -g -mabi=32 -ffreestanding -mfix4300 -I/usr/include/n64 -I/usr/include/n64/PR -Iinclude -Ibuild -Ibuild/include -Isrc \
 -D_FINALROM=1 -DNDEBUG=1 -DF3DEX_GBI_2=1 -MMD -MF build/mem_heap.d  -o build/mem_heap.o mem_heap.c
 
 make
