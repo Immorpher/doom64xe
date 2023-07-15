2e819877495f80eea0162e62d57d1f57f8363bc5
Immorpher <58839754+Immorpher@users.noreply.github.com>
2023-07-15 05:30:20 -0900

    Expand System Stack Sizes
    Some of the stack sizes for basic system processes had to be expanded to run on hardware

--- doom64/src/i_main.c
+++ doom64/src/i_main.c
@@ -23,8 +23,9 @@ extern char _codeSegmentEnd[];
 
 
 extern u32 cfb[2][SCREEN_WD*SCREEN_HT]; // 8036A000
+
 /* Nova, this stack size is in bytes */
-#define	BOOT_STACKSIZE	0x100
+#define	BOOT_STACKSIZE	0x200
 
 u64	bootStack[BOOT_STACKSIZE/sizeof(u64)];
 
@@ -40,7 +41,7 @@ extern int globalcm;   // 800A68fC r_local.h
 #define SYS_THREAD_ID_MAIN 2
 #define SYS_THREAD_ID_TICKER 3
 
-#define SYS_IDLE_STACKSIZE 0x400
+#define SYS_IDLE_STACKSIZE 0x1000
 OSThread	idle_thread;                        // 800A4A18
 u64	idle_stack[SYS_IDLE_STACKSIZE/sizeof(u64)]; // 800979E0
 
@@ -48,7 +49,7 @@ u64	idle_stack[SYS_IDLE_STACKSIZE/sizeof(u64)]; // 800979E0
 OSThread	main_thread;                        // 800A4BC8
 u64	main_stack[SYS_MAIN_STACKSIZE/sizeof(u64)]; // 80099A00
 
-#define SYS_TICKER_STACKSIZE 0x800
+#define SYS_TICKER_STACKSIZE 0x1000
 OSThread	sys_ticker_thread;                          // 800A4D78
 u64	sys_ticker_stack[SYS_TICKER_STACKSIZE/sizeof(u64)]; // 800A3A00
 
