5fffb41e538ec588b3bdd039cfe84052605af849
Immorpher <58839754+Immorpher@users.noreply.github.com>
2023-10-01 17:51:47 -0400

    Beta 64 animation list
    Getting ready for Beta 64 animations

--- doom64/src/p_spec.c
+++ doom64/src/p_spec.c
@@ -32,6 +32,33 @@ animdef_t		animdefs[MAXANIMS] = // 8005AE80
 	{  0,  "HTELA", 4, 1,  true, false }
 };
 
+/*---------- Beta 64 table for the future
+animdef_t		animdefs[MAXANIMS] = // 8005AE80
+{
+	{ 15, "SMONAA", 4, 7, false, false },
+	{  0, "SMONBA", 4, 1, false, false },
+	{  0, "SMONCA", 4, 7, false, false },
+	{ 90, "CFACEA", 3, 3,  true, false },
+	{  0, "SMONDA", 4, 3, false, false },
+	{ 10, "SMONEA", 4, 7, false, false },
+	{  0, "SPORTA", 9, 3, false,  true },
+	{ 10,  "SMONF", 5, 1,  true,  true },
+	{ 10, "STRAKR", 5, 1,  true,  true },
+	{ 10, "STRAKB", 5, 1,  true,  true },
+	{ 10, "STRAKY", 5, 1,  true,  true },
+	{ 50,  "C307B", 5, 1,  true,  true },
+	{  0,   "CTEL", 8, 3, false,  true },
+	{  0,"CASFL98", 5, 7,  true,  true },
+	{  0,  "HTELA", 4, 1,  true, false },
+	{  0,  "LASERT", 2, 1,  false, false },
+	{  0,  "LASERG1", 2, 1,  false, false },
+	{  0,  "LASERG2", 2, 1,  false, false },
+	{  0,  "LASERG", 2, 1,  false, false },
+	{  0,  "WFALL1", 8, 2,  false, false }
+};
+/---------*/
+
+
 /*----------
 / anims[8] -> anims[MAXANIMS = 15]
 / For some reason Doom 64 is 8,
