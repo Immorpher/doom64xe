2b5a239b4594cc80905ed41ddb9c73bd7f33ce9b
Immorpher <58839754+Immorpher@users.noreply.github.com>
2024-01-31 22:06:29 -0500

    Improved Automap Rendering
    From Nova's Doom 64 ultra this adds clipping and GBA Doom stack rendering to the automap so it prevents crashing and lagging on large maps.

--- doom64/src/m_bbox.c
+++ doom64/src/m_bbox.c
@@ -19,3 +19,11 @@ void M_AddToBox(fixed_t *box, fixed_t x, fixed_t y) // 80000470
 	if (y>box[BOXTOP])
 		box[BOXTOP] = y;
 }
+
+// From Nova, used to clip the automap so it runs efficiently.
+
+boolean M_BoxIntersect(fixed_t a[static 4], fixed_t b[static 4])
+{
+    return a[BOXLEFT] < b[BOXRIGHT] && a[BOXRIGHT] > b[BOXLEFT]
+        && a[BOXBOTTOM] < b[BOXTOP] && a[BOXTOP] > b[BOXBOTTOM];
+}
